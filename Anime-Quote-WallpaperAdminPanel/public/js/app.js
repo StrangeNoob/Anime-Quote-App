b4a083ed23cde0d9392ff665cecc1ba29fbc36d0
StrangeNoob <sipun63427452@gmail.com>
2019-06-15 00:24:25 +0530

    Firebase Auth Not Working

--- Anime-Quote-WallpaperAdminPanel/public/js/app.js
+++ Anime-Quote-WallpaperAdminPanel/public/js/app.js
@@ -1,25 +1,32 @@
+// Web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyADB7FqX6lBeCHNLPqqmNbG8z_oDIEw0uw",
-    authDomain: "wallpaper-app-adbc8.firebaseapp.com",
-    databaseURL: "https://wallpaper-app-adbc8.firebaseio.com",
-    projectId: "wallpaper-app-adbc8",
-    storageBucket: "wallpaper-app-adbc8.appspot.com",
-    messagingSenderId: "680703404288",
-    appId: "1:680703404288:web:58d44f6aae404386"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.auth.Auth.Persistence.LOCAL; 
+  apiKey: "AIzaSyADB7FqX6lBeCHNLPqqmNbG8z_oDIEw0uw",
+  authDomain: "wallpaper-app-adbc8.firebaseapp.com",
+  databaseURL: "https://wallpaper-app-adbc8.firebaseio.com",
+  projectId: "wallpaper-app-adbc8",
+  storageBucket: "wallpaper-app-adbc8.appspot.com",
+  messagingSenderId: "680703404288",
+  appId: "1:680703404288:web:58d44f6aae404386"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.auth.Auth.Persistence.LOCAL; 
+
 $(document).ready(function(){
 $("#btn-login").click( function(){
-    alert("You Clicked");
+
+    alert("Hi User !!!");
     var  email = $("#email").val();
     var password = $("#password").val();
-    var result = firebase.auth().signInWithEmailAndPassword(email,password);
-    result.catch(function(error){
-        var errorCode =  error.code;
-        var errorMessage =  error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
+    console.log(password);
+    console.log(email);
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // To Print Error in Console
+      console.log(errorCode);
+      console.log(errorMessage);
     });
   });
 });
